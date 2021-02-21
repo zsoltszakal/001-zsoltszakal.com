1777853bae54b89200991c3008a0175e30eebb21
Zsolt Szakal <zsltszakal@gmail.com>
2021-02-21 19:34:06 +0000

    Color and Font change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,10 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 const Navbar = () => { 
     return (
         <nav className="navbar navbar-expand-lg navbar-light navbar-custom fixed-top">
+        <div className="container">
             <a className="navbar-brand" href="https://zsoltszakal.com/">Zsolt Szakal</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <FontAwesomeIcon icon={ faBars } style={{color: "#27ae60"}}/>
+                <FontAwesomeIcon icon={ faBars } style={{color: "#ffd369"}}/>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ml-auto">
@@ -30,6 +31,7 @@ const Navbar = () => {
                      <a className="nav-link" href="#">Contact</a>                        
                  </li>
                 </ul>   
+                </div>
                 </div>
 </nav>
     )
