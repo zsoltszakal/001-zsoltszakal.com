140f965b5316a354e6735e64954ec3197cdca183
Zsolt Szakal <zsltszakal@gmail.com>
2021-02-22 19:35:27 +0000

    Colors 2.0

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,18 +14,18 @@ const Skills = () => {
                         <h3 className="projecth3">zNote Project</h3>
                         <h3 className="projecth4">Simple note taking app without a database using React</h3>
                         <img className="ppic" src= { p1pic } alt="Project1Pic"></img>
-                        <h3 className="projecth5">GitHub</h3>
-                        <h3 className="projecth5">Live Demo</h3>
+                        <h3 className="projecth5"><a href="" target="_blank">GitHub</a></h3>
+                        <h3 className="projecth5"><a href="" target="_blank">Live Demo</a></h3>
                     </div>
                 </div>
                 <div className="col-lg-6 col-md-12 col-sm-12 col-12">
                     <div className="box">
                         <div className="circle"></div>
                         <h3 className="projecth3">Portfolio Website</h3>
-                        <h3 className="projecth4">Simple note taking app without a database using React</h3>
+                        <h3 className="projecth4">Personal portfolio website with React and Bootstrap</h3>
                         <img className="ppic" src= { p2pic } alt="Project1Pic"></img>
-                        <h3 className="projecth5">GitHub</h3>
-                        <h3 className="projecth5">Live Demo</h3>
+                        <h3 className="projecth5"><a href="https://github.com/zsoltszakal/001-zsoltszakal.com" target="_blank">GitHub</a></h3>
+                        <h3 className="projecth5"><a href="https://zsoltszakal.com/" target="_blank">Live Demo</a></h3>
                     </div>
                 </div>
                 <div className="col-lg-6 col-md-12 col-sm-12 col-12">
@@ -34,8 +34,8 @@ const Skills = () => {
                         <h3 className="projecth3">CloudGuruChallenge</h3>
                         <h3 className="projecth4">Simple note taking app without a database using React</h3>
                         <img className="ppic" src= { p1pic } alt="Project1Pic"></img>
-                        <h3 className="projecth5">GitHub</h3>
-                        <h3 className="projecth5">Live Demo</h3>
+                        <h3 className="projecth5"><a href="" target="_blank">GitHub</a></h3>
+                        <h3 className="projecth5"><a href="" target="_blank">Live Demo</a></h3>
                     </div>
                 </div>
                 <div className="col-lg-6 col-md-12 col-sm-12 col-12">
@@ -44,8 +44,8 @@ const Skills = () => {
                         <h3 className="projecth3">OAuth & MongoDB</h3>
                         <h3 className="projecth4">Simple note taking app without a database using React</h3>
                         <img className="ppic" src= { p1pic } alt="Project1Pic"></img>
-                        <h3 className="projecth5">GitHub</h3>
-                        <h3 className="projecth5">Live Demo</h3>
+                        <h3 className="projecth5"><a href="" target="_blank">GitHub</a></h3>
+                        <h3 className="projecth5"><a href="" target="_blank">Live Demo</a></h3>
                     </div>
                 </div>
                 
