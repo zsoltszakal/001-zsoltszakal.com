7f914049f331b22fc58d3cd6bd2c3601d5d523aa
Zsolt Szakal <zsltszakal@gmail.com>
2021-02-22 13:58:06 +0000

    Skills 2.0

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,69 +9,70 @@ const Skills = () => {
             <h1 className="py-5">Skills</h1>
                 <div className="container">
                 <div className="row">
-                    <div className="col-lg-4 col-md-6 col-sm-12">
+                    <div className="col-lg-3 col-md-6 col-sm-6 col-6">
                     <div className="box">
                         <div className="circle"><FontAwesomeIcon className="icon" icon={faPython} size="3x" /></div>
                         <h3 className="skill">python</h3>
                         <h3 className="skill-s">⭐⭐☆☆</h3>
                     </div>
                 </div>
-                <div className="col-lg-4 col-md-6 col-sm-12">
+                <div className="col-lg-3 col-md-6 col-sm-6 col-6">
                     <div className="box">
                         <div className="circle"><FontAwesomeIcon className="icon" icon={faReact} size="3x" /></div>
                         <h3 className="skill">react</h3>
                         <h3 className="skill-s">⭐⭐☆☆</h3>
                     </div>
                 </div>
-                <div className="col-lg-4 col-md-6 col-sm-12">
+                <div className="col-lg-3 col-md-6 col-sm-6 col-6">
                     <div className="box">
                         <div className="circle"><FontAwesomeIcon className="icon" icon={faEnvira} size="3x" /></div>
                         <h3 className="skill">mongodb</h3>
                         <h3 className="skill-s">⭐⭐☆☆</h3>
                     </div>
                 </div>
-                <div className="col-lg-4 col-md-6 col-sm-12">
+                <div className="col-lg-3 col-md-6 col-sm-6 col-6">
                     <div className="box">
                         <div className="circle"><FontAwesomeIcon className="icon" icon={faDatabase} size="3x" /></div>
                         <h3 className="skill">sql</h3>
                         <h3 className="skill-s">⭐⭐☆☆</h3>
                     </div>
                 </div>
-                <div className="col-lg-4 col-md-6 col-sm-12">
+                <div className="col-lg-3 col-md-6 col-sm-6 col-6">
                     <div className="box">
                         <div className="circle"><FontAwesomeIcon className="icon" icon={faAws} size="3x" /></div>
                         <h3 className="skill">aws</h3>
                         <h3 className="skill-s">⭐⭐☆☆</h3>
                     </div>
                 </div>
-                <div className="col-lg-4 col-md-6 col-sm-12">
+                {/* <div className="col-lg-4 col-md-6 col-sm-6 col-6">
                     <div className="box">
                         <div className="circle"><FontAwesomeIcon className="icon" icon={faCloud} size="3x" /></div>
                         <h3 className="skill">gcs</h3>
                         <h3 className="skill-s">⭐⭐☆☆</h3>
                     </div>
-                </div>
-                <div className="col-lg-4 col-md-6 col-sm-12">
+                </div> */}
+                <div className="col-lg-3 col-md-6 col-sm-6 col-6">
                     <div className="box">
                         <div className="circle"><FontAwesomeIcon className="icon" icon={faDocker} size="3x" /></div>
                         <h3 className="skill">docker</h3>
                         <h3 className="skill-s">⭐⭐☆☆</h3>
                     </div>
                 </div>
-                <div className="col-lg-4 col-md-6 col-sm-12">
+                <div className="col-lg-3 col-md-6 col-sm-6 col-6">
                     <div className="box">
                         <div className="circle"><FontAwesomeIcon className="icon" icon={faGitAlt} size="3x" /></div>
                         <h3 className="skill">git</h3>
                         <h3 className="skill-s">⭐⭐⭐☆</h3>
                     </div>
                 </div>
-                <div className="col-lg-4 col-md-6 col-sm-12">
+                <div className="col-lg-3 col-md-6 col-sm-6 col-6 center">
                     <div className="box">
                         <div className="circle"><FontAwesomeIcon className="icon" icon={faTerminal} size="3x" /></div>
                         <h3 className="skill">cli</h3>
                         <h3 className="skill-s">⭐⭐⭐☆</h3>
                     </div>
                 </div>
+                
                 <div className="skill-b"></div>
                 
                
